ad9ad0c30d50da1c1b1c2e1c3936e9684986475d
Moe Sani <moefattahi.sani@dyson.com>
2023-07-23 23:02:27 +0100

    added message for opening closing

--- threefinger_publisher/test_publisher.py
+++ threefinger_publisher/test_publisher.py
@@ -19,20 +19,26 @@ from std_msgs.msg import String
 from example_interfaces.msg import Float64MultiArray
 
 class MinimalPublisher(Node):
-
+    CLOSE_POSITION = [ -1000, -2000, -3000]
+    OPEN_POSITION = [ 15000, 12000, 40000]
+    isOpen = False
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(Float64MultiArray, '/joint_angles', 10)
-        timer_period = 1  # seconds
+        timer_period = 5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
 
     def timer_callback(self):
-        msg = Float64MultiArray(data = [self.i, self.i, self.i])
+        if self.isOpen:
+            msg = Float64MultiArray(data = self.CLOSE_POSITION)
+            self.isOpen = False
+        else:
+            msg = Float64MultiArray(data = self.OPEN_POSITION)
+            self.isOpen = True
+
         # msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 10000
 
 
 def main(args=None):
